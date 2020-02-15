ae820bae1270e97b66804c1619f0c9226af8a880
Paul Sterl <paul.sterl@outlook.de>
2020-02-15 16:16:24 +0100

    using shared module

--- frontend/src/app/identity/service/identity.service.ts
+++ frontend/src/app/identity/service/identity.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Pageable,HateoasEntityList,HateoasResourceLinks, Sort } from '@sterlp/ng-spring-boot-api';
-import { Observable, BehaviorSubject, of, Subscription } from 'rxjs';
-import { catchError, map, tap, finalize } from 'rxjs/operators';
-import { CollectionViewer, DataSource } from '@angular/cdk/collections';
 import { IdentityList, Identity } from '../api/identity-api.model';
-import { SpringResoureceService, SpringDataSource, DataSourceErrorHandler } from 'src/app/shared/public-api';
+import { SpringResourece,
+    HateoasEntityList,
+    HateoasResourceLinks,
+    DataSourceErrorHandler,
+    SpringDataSource } from '@sterlp/ng-spring-boot-api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IdentityService
-    extends SpringResoureceService<HateoasEntityList<IdentityList,HateoasResourceLinks>, Identity> {
+    extends SpringResourece<HateoasEntityList<IdentityList, HateoasResourceLinks>, Identity> {
     constructor(http: HttpClient) { super(http); }
     get listUrl() { return '/api/identities'; }
 }
 
-export class IdentityDataSource extends SpringDataSource<HateoasEntityList<IdentityList,HateoasResourceLinks>, Identity> {
+export class IdentityDataSource extends SpringDataSource<HateoasEntityList<IdentityList, HateoasResourceLinks>, Identity> {
     constructor(identityService: IdentityService, errorHandler?: DataSourceErrorHandler) {
         super(identityService, errorHandler);
     }
-    extractDataFromList(list:HateoasEntityList<IdentityList,HateoasResourceLinks>): Identity[] {
+    extractDataFromList(list: HateoasEntityList<IdentityList, HateoasResourceLinks>): Identity[] {
         return list && list._embedded && list._embedded.identities ? list._embedded.identities : null;
     }
 }
--- frontend/src/app/system/service/system.service.ts
+++ frontend/src/app/system/service/system.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { SpringResoureceService } from 'src/app/shared/public-api';
 import { SystemCredential, SystemCredentialHateoasList } from '../api/system-api.model';
 import { HttpClient } from '@angular/common/http';
+import { SpringResourece } from '@sterlp/ng-spring-boot-api';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SystemService extends SpringResoureceService<SystemCredentialHateoasList, SystemCredential> {
+export class SystemService extends SpringResourece<SystemCredentialHateoasList, SystemCredential> {
     constructor(http: HttpClient) { super(http); }
-    get listUrl(): string { return 'api/system-credentials'; }
+    get listUrl(): string { return '/api/system-credentials'; }
 }
